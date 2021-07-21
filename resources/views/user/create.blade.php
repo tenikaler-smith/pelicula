1382d874bd36681d8397093e5e56ee1f56036e22
iampitufo <tenikaler.smith@utp.ac.pa>
2021-07-21 03:58:51 -0500

    TODO: VIEWS: creación de la vista create para user

--- resources/views/user/create.blade.php
+++ resources/views/user/create.blade.php
@@ -1,113 +1,97 @@
 @extends('plantilla.layout')
 
     @section('titulo')
-        Taller 7 - Crear Usuario
+        Crear Usuario
     @endsection
 
     @section('contenido')
       <br>
 
-    <div class="card">
-      <div class="card-body">
-        <div class="text-center m-auto">
-          <h4 class="text-uppercase text-center">Crear Usuario</h4>
-        </div>
-
-
-
 <section class="row">
-            <article class="col">
-                <div class="card">
+    <div class="container mt-5 pt-5 mb-5">
+        <div class="row justify-content-center">
+            <div class="col-md-6 col-lg-4 col-xl-4">
+
+                <div id="alertSuccess">
+                    @if (session('estado'))
+                    <div class="alert alert-warning alert-dismissible fade show" role="alert">
+                        <strong>{{ session('estado') }}</strong>
+                        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                    </div>
+                    @endif
+                </div>
+
+                <div id="alertSuccess">
+                    @if (session('success'))
+                    <div class="alert alert-success alert-dismissible fade show" role="alert">
+                        <strong>{{ session('success') }}</strong>
+                        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                    </div>
+                    @endif
+                </div>
+
+
+                <div class="card bg-light">
                     <div class="card-body">
-                        <div class="text-center m-auto">
+                        <div class="text-center m-auto btn-">
                             <h4 class="text-uppercase text-center">Crear Usuario</h4>
                         </div>
-                        <div id="alertDanger">
-                            @if (session('danger'))
-                            <div class="alert alert-danger alert-dismissible fade show" role="alert" id="#myAlert">
-                                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-                                <strong>{{ session('danger') }}</strong>
-                            </div>
-                            @endif
-                        </div>
 
-                        <form action="{{ Route('user.create') }}" method="post" role="form">
+                        <form action="{{ Route('user.store') }}" method="POST" class="row g-3" role="form">
                             @csrf
 
-                            <div class="row mb-3">
-                                <label for="txtTitle" class="col-sm-2 form-label">Título de la Pelicula</label>
-                                <div class="row col-sm-6">
-                                    <input type="txtTitle" name="txtTitle" value="{{old('txtTitle')}}" id="txtTitle"
-                                        class="form-control" placeholder="Título">
+                            <div class="form-group mb-3">
+                                <label for="txtNombre" class="form-label"> Nombre </label>
+                                <input type="text" name="txtNombre" placeholder="Nombre"
+                                    class="form-control" id="txtNombre" value="{{old('txtNombre')}}"
+                                    aria-describedby="inputGroupPrepend">
+                            </div>
+
+                            <div class="form-group mb-3">
+                                <label for="txtUser" class="form-label"> Usuario </label>
+                                <input type="text" name="txtUser" placeholder="Usuario"
+                                    class="form-control" id="txtUser" value="{{old('txtUser')}}"
+                                    aria-describedby="inputGroupPrepend">
+                            </div>
+
+                            <div class="form-group mb-3">
+                                <label for="txtPassword"> Contraseña</label>
+                                <div class="input-group bg-light">
+                                    <input type="password" class="form-control" id="txtPassword" value="{{old("txtPassword")}}" name="txtPassword"
+                                        placeholder="contraseña" aria-describedby="inputGroupPrepend">
+                                </div>
+                                <div id="alertSuccess">
+                                  @if (session('estado2'))
+                                  <div class="text-danger">{{ session('estado2') }}
+                                  </div>
+                                  @endif
                                 </div>
                             </div>
 
-                            <div class="row mb-3">
-                                <label for="txtTitle" class="col-sm-2 form-label">Género</label>
-                                <div class="row col-sm-6">
-                                    <select name="selGenero" class=" col-sm-6 form-select dropdown form-control"
-                                        aria-label="genero">
-                                        @foreach ($generos as $clave=>$value)
-                                        <option class="dropdown-item" value="{{$clave}}">{{$value}}</option>
-                                        @endforeach
-                                    </select>
+                            <div class="form-group mb-3">
+                                <label for="txtPassword2"> Repetir Contraseña</label>
+                                <div class="input-group bg-light">
+                                    <input type="password" class="form-control" id="txtPassword2" value="{{old("txtPassword2")}}" name="txtPassword2"
+                                        placeholder="Repetir Contraseña" aria-describedby="inputGroupPrepend2">
+                                </div>
+                                <div id="alertSuccess">
+                                  @if (session('estado2'))
+                                  <div class="text-danger">{{ session('estado2') }}
+                                  </div>
+                                  @endif
                                 </div>
                             </div>
 
-                            <div class="row mb-3">
-                                <label for="txtResumen" class="col-sm-2 form-label">Resumen</label>
-                                <div class="row col-sm-6">
-                                    <input type="txtResumen" name="txtResumen" value="{{old('txtResumen')}}" id="txtResumen"
-                                        class="form-control" placeholder="Resumen">
-                                </div>
+                            <div class="form-group mb-0 text-center">
+                                <button class="btn btn-warning btn-block" type="submit" name="submit"> Registrar
+                                </button> |
+                                <button class="btn btn-dark btn-block" type="reset" name="reset"> Cancelar
+                                </button>
                             </div>
-
-                            <div class="row mb-3">
-                                <label for="numPrecio" class="col-sm-2 form-label">Precio</label>
-                                <div class="row col-sm-6">
-                                    <input type="numPrecio" name="numPrecio" value="{{old('numPrecio')}}" id="numPrecio"
-                                        class="form-control" placeholder="Precio">
-                                </div>
-                            </div>
-
-                            <div>
-                                <button type="submit" class="btn btn-primary">procesar</button>|<button type="reset"
-                                    class="btn btn-warning">Borrar</button>
-                            </div>
-
                         </form>
-
                     </div>
                 </div>
-            </article>
-        </section>
-        <br>
-        <hr>
-        <br>
-        <section>
-            <article class="col" id="CardCalcularPrecio4">
-                @if (session('success'))
-                <div>
-                    <div class="card border-success mb-3 w-50">
-                        <div class="card-header bg-transparent border-success">
-                            <h5 class="card-title">El resultado de la operación</h5>
-                        </div>
-                        <div class="card-body text-success">
-                            <p class="card-text"><strong>Datos Guardados</strong></p>
-                            <p class="card-text"><strong>{{ session('success') }}</strong></p>
-                        </div>
-                        <div class="footer">
-                            <button id="problema9" class="btn btn-warning">Limpiar</button>
-                        </div>
-                    </div>
-                </div>
-                @endif
-            </article>
-        </section>
-
-
-
-      </div>
+            </div>
+        </div>
     </div>
-    <br/>
   @endsection
