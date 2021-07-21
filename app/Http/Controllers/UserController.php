028d9b39f3b5b509cc87c20dfe209c900bf3b13d
iampitufo <tenikaler.smith@utp.ac.pa>
2021-07-21 03:52:22 -0500

    TODO: CONTROLLER: Mejoras en las funciones de edit, store, create...

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Redis;
+use App\Models\User;
 
 class UserController extends Controller
 {
@@ -58,21 +58,45 @@ class UserController extends Controller
      */
     public function store(Request $request)
     {
+        if($request->txtPassword != $request->txtPassword2)
+            return back()->with("estado2", "Las contraseñas no coinciden");
 
-        $arrDatos = array($request->txtTitle, $request->selGenero, $request->txtResumen, $request->numPrecio);
+        if($request->txtPassword=="" || $request->txtPassword2=="")
+            return back()->with("estado2", "Las contraseñas no pueden estar vacias");
 
-        if ($request->txtTitle == "" and $request->selGenero == "" and $request->txtResumen == "" and $request->numPrecio == "") {
-            return redirect()->route("")->with("danger", "debes llenar los todos datos")->withInput();
-        } else {
-            if ($request->selGenero == "0") {
-                return redirect()->route("user.create")->with("danger", "debes seleccinar un Genero")->withInput();
-            } else {
+        $resultados = User::get()->where('usuario', $request->txtUser);
+        $encontrado = 0;
 
-                foreach ($arrDatos as $valor) {
-                    return redirect()->route("user.create")->with("success", $valor)->withInput();
-                }
-            }
-        }
+        foreach ($resultados as $resultado)
+            $encontrado = 1;
+
+        if($encontrado == 1)
+            return back()->with("estado", "El Usuario ya existe")->withInput();
+
+        $obj = new User();
+        $obj->usuario = $request->txtUser;
+        $obj->password = md5($request->txtPassword);
+        $obj->nombre = $request->txtNombre;
+        $obj->rol = "User";
+        $obj->save();
+        return back()->with("success", "Usuario creado con éxito");
+
+
+
+        // $arrDatos = array($request->txtTitle, $request->selGenero, $request->txtResumen, $request->numPrecio);
+
+        // if ($request->txtTitle == "" and $request->selGenero == "" and $request->txtResumen == "" and $request->numPrecio == "") {
+        //     return redirect()->route("")->with("danger", "debes llenar los todos datos")->withInput();
+        // } else {
+        //     if ($request->selGenero == "0") {
+        //         return redirect()->route("user.create")->with("danger", "debes seleccinar un Genero")->withInput();
+        //     } else {
+
+        //         foreach ($arrDatos as $valor) {
+        //             return redirect()->route("user.create")->with("success", $valor)->withInput();
+        //         }
+        //     }
+        // }
     }
 
 
@@ -82,40 +106,68 @@ class UserController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function edit($id)
+    public function edit()
     {
-        //
+        if(session("rol")== "admin" || session("rol")== "User")
+            return view("user.edit");
+        else
+            return redirect("noaccess");
+
+        // $usuario = User::find($id_user);
     }
 
 
     public function login_post(Request $request)
     {
-        if ($request->txtUser == "" and $request->txtPassword == "") {
-            return redirect()->route("user.login_post")->with("estado", "debes llenar los datos")->withInput();
-        } else {
-            if (($request->txtUser == "tenismith" || $request->txtUser == "admin") and ($request->txtPassword == "12345")) {
-                session(["nombre" => $request->txtUser]);
-                session(["rol" => $request->txtUser]);
+
+        $resultados = User::get()
+                            ->where('usuario', $request->txtUser)
+                            ->where('password', md5($request->txtPassword));
+
+        if($resultados == "[]"){
+            return redirect()->route("user.login")->with("estado", "debes llenar los datos")->withInput();
+        }else{
+            foreach ($resultados as $resultado) {
+                session(['id_user' => $resultado->id_user]);
+                session(['nombre' => $resultado->nombre]);
+                session(['usuario' => $resultado->usuario]);
+                session(['rol' => $resultado->rol]);
+                session(['password' => $resultado->password]);
                 return redirect()->route("catalog.index");
-            } else {
-                if ($request->txtUser == "tenismith23" || $request->txtUser == "admin") {
-                    return redirect()->route('user.login_post')->with("estado2", "La contraseña no es correcta. Compruébala.")->withInput();
-                } else {
-                    return redirect()->route('user.login_post')->with("estado1", "El nombre de usuario que ingresaste no pertenece a ninguna cuenta. Comprueba el nombre de usuario y vuelve a intentarlo.")->withInput();
-                }
             }
-
-            // if($request->txtUser == "tenismith23"){
-            //     if($request->txtPassword == "12345"){
-            //         return redirect()->route('catalog.index');
-            //     }else{
-            //         return redirect()->route('user.login_post')->with("estado2", "La contraseña no es correcta. Compruébala.")->withInput();
-            //     }
-            // }else{
-            //     return redirect()->route('user.login_post')->with("estado1", "El nombre de usuario que ingresaste no pertenece a ninguna cuenta. Comprueba el nombre de usuario y vuelve a intentarlo.")->withInput();
-            // }
-
         }
+
+
+        /*
+            if ($request->txtUser == "" and $request->txtPassword == "") {
+                return redirect()->route("user.login_post")->with("estado", "debes llenar los datos")->withInput();
+            } else {
+                if (($request->txtUser == "tenismith" || $request->txtUser == "admin") and ($request->txtPassword == "12345")) {
+                    session(["nombre" => $request->txtUser]);
+                    session(["rol" => $request->txtUser]);
+                    return redirect()->route("catalog.index");
+                } else {
+                    if ($request->txtUser == "tenismith23" || $request->txtUser == "admin") {
+                        return redirect()->route('user.login_post')->with("estado2", "La contraseña no es correcta. Compruébala.")->withInput();
+                    } else {
+                        return redirect()->route('user.login_post')->with("estado1", "El nombre de usuario que ingresaste no pertenece a ninguna cuenta. Comprueba el nombre de usuario y vuelve a intentarlo.")->withInput();
+                    }
+                }
+        */
+
+        /*
+            if($request->txtUser == "tenismith23"){
+                if($request->txtPassword == "12345"){
+                    return redirect()->route('catalog.index');
+                }else{
+                    return redirect()->route('user.login_post')->with("estado2", "La contraseña no es correcta. Compruébala.")->withInput();
+                }
+            }else{
+                return redirect()->route('user.login_post')->with("estado1", "El nombre de usuario que ingresaste no pertenece a ninguna cuenta. Comprueba el nombre de usuario y vuelve a intentarlo.")->withInput();
+            }
+        }
+        */
+
     }
 
     public function logout()
