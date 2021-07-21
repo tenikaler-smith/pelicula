b9ba2b6b101ab18905f427c4499fcc19005dad0f
iampitufo <tenikaler.smith@utp.ac.pa>
2021-07-21 04:01:24 -0500

    TODO: VIEWS: USER: Vista para editar Usuario

---
+++ resources/views/user/edit.blade.php
@@ -0,0 +1,95 @@
+@extends('plantilla.layout')
+
+@section('titulo')
+Taller 7 - Editar Usuario
+@endsection
+
+@section('contenido')
+<br>
+
+<section class="row">
+    <div class="container mt-5 pt-5 mb-5">
+        <div class="row justify-content-center">
+            <div class="col-md-6 col-lg-4 col-xl-4">
+
+                <div id="alertSuccess">
+                    @if (session('estado'))
+                    <div class="alert alert-warning alert-dismissible fade show" role="alert">
+                        <strong>{{ session('estado') }}</strong>
+                        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                    </div>
+                    @endif
+                </div>
+
+                <div id="alertSuccess">
+                    @if (session('success'))
+                    <div class="alert alert-success alert-dismissible fade show" role="alert">
+                        <strong>{{ session('success') }}</strong>
+                        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                    </div>
+                    @endif
+                </div>
+
+
+                <div class="card bg-light">
+                    <div class="card-body">
+                        <div class="text-center m-auto btn-">
+                            <h4 class="text-uppercase text-center"> Editar Usuario </h4>
+                        </div>
+
+                        <form action="{{ Route('user.store') }}" method="POST" class="row g-3" role="form">
+                            @csrf
+
+                            <div class="form-group mb-3">
+                                <label for="txtNombre" class="form-label"> Nombre </label>
+                                <input type="text" name="txtNombre" placeholder="Nombre" class="form-control"
+                                    id="txtNombre" value="{{session('nombre')}}" aria-describedby="inputGroupPrepend">
+                            </div>
+
+                            <div class="form-group mb-3">
+                                <label for="txtUser" class="form-label"> Usuario </label>
+                                <input type="text" name="txtUser" placeholder="Usuario" class="form-control"
+                                    id="txtUser" value="{{session('usuario')}}" aria-describedby="inputGroupPrepend">
+                            </div>
+
+                            <div class="form-group mb-3">
+                                <label for="txtPassword"> Contraseña </label>
+                                <div class="input-group bg-light">
+                                    <input type="password" class="form-control" id="txtPassword"
+                                        value="" name="txtPassword" placeholder="contraseña"
+                                        aria-describedby="inputGroupPrepend">
+                                </div>
+                                <div id="alertSuccess">
+                                    @if (session('estado2'))
+                                    <div class="text-danger">{{ session('estado2') }}
+                                    </div>
+                                    @endif
+                                </div>
+                            </div>
+
+                            <div class="form-group mb-3">
+                                <label for="txtPassword2"> Repetir Contraseña </label>
+                                <div class="input-group bg-light">
+                                    <input type="password" class="form-control" id="txtPassword2"
+                                        value="" name="txtPassword2"
+                                        placeholder="Repetir Contraseña" aria-describedby="inputGroupPrepend2">
+                                </div>
+                                <div id="alertSuccess">
+                                    @if (session('estado2'))
+                                    <div class="text-danger">{{ session('estado2') }}
+                                    </div>
+                                    @endif
+                                </div>
+                            </div>
+
+                            <div class="form-group mb-0 text-center">
+                                <button class="btn btn-warning btn-block" type="submit" name="submit"> Guardar </button> |
+                                <button type="reset" name="reset" class="btn btn-dark btn-block"> Cancelar </button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    @endsection
