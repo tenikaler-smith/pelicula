c4bea65cb0453819a30383d937e73844db279101
iampitufo <tenikaler.smith@utp.ac.pa>
2021-07-21 03:55:35 -0500

    TODO: DATABASE: Cambios en la base de datos

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,12 +14,11 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->increments('id_user');
+            $table->string('usuario', 20);
+            $table->string('password', 50);
+            $table->string('nombre', 50);
+            $table->string('rol', 20);
             $table->timestamps();
         });
     }
